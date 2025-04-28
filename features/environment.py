38b6229c7aa07d4177a9ae0809592e1a90d9ef6f
Pranjal Melkani <pranjal.melkani18@gmail.com>
2025-04-28 16:08:43 +0530

    basedriver added in environment file

---
+++ features/environment.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+
+from base.basedriver import Basedriver
+
+
+def before_all(context):
+    context.driver = webdriver.Chrome()
+    context.driver.maximize_window()
+    context.driver.get("https://automationexercise.com/")
+    context.base = Basedriver(context.driver)
+    
+def after_all(context):
+    context.driver.quit()    
